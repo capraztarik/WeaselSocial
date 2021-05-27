fc895350e5a1d361913491e730f61bd5ff7dfa25
Abidin Gokcekaya <agokcekaya@sabanciuniv.edu>
2021-05-27 11:59:56 +0300

    Fixed upload image page.

--- lib/Screens/share_post.dart
+++ lib/Screens/share_post.dart
@@ -6,14 +6,13 @@ import 'package:uuid/uuid.dart';
 import 'dart:async';
 import 'dart:io';
 
+File file;
+
 class Uploader extends StatefulWidget {
   _Uploader createState() => _Uploader();
 }
 
 class _Uploader extends State<Uploader> {
-
-  File file;
-  ImagePicker imagePicker = ImagePicker();
   TextEditingController descriptionController = TextEditingController();
   TextEditingController locationController = TextEditingController();
   bool uploading = false;
@@ -25,23 +24,19 @@ class _Uploader extends State<Uploader> {
 
   //method to get Location and save into variables
   Widget build(BuildContext context) {
-    return file == null
-        ? IconButton(
-        icon: Icon(Icons.file_upload),
-        onPressed: () => {_selectImage(context)})
-        : Scaffold(
+    return Scaffold(
         resizeToAvoidBottomInset: false,
         appBar: AppBar(
           backgroundColor: Colors.white70,
           leading: IconButton(
-              icon: Icon(Icons.arrow_back, color: Colors.black),
+              icon: Icon(Icons.clear, color: Colors.black),
               onPressed: clearImage),
           title: const Text(
-            'Post to',
+            'New Post',
             style: const TextStyle(color: Colors.black),
           ),
           actions: <Widget>[
-            FlatButton(
+            TextButton(
                 onPressed: postImage,
                 child: Text(
                   "Post",
@@ -55,7 +50,6 @@ class _Uploader extends State<Uploader> {
         body: ListView(
           children: <Widget>[
             PostForm(
-              imageFile: file,
               descriptionController: descriptionController,
               loading: uploading,
             ),
@@ -64,47 +58,6 @@ class _Uploader extends State<Uploader> {
   }
 
   //method to build buttons with location.
-
-  _selectImage(BuildContext parentContext) async {
-    return showDialog<Null>(
-      context: parentContext,
-      barrierDismissible: false, // user must tap button!
-
-      builder: (BuildContext context) {
-        return SimpleDialog(
-          title: const Text('Create a Post'),
-          children: <Widget>[
-            SimpleDialogOption(
-                child: const Text('Take a photo'),
-                onPressed: () async {
-                  Navigator.pop(context);
-                  PickedFile imageFile =
-                  await imagePicker.getImage(source: ImageSource.camera, maxWidth: 1920, maxHeight: 1200, imageQuality: 80);
-                  setState(() {
-                    file = File(imageFile.path);
-                  });
-                }),
-            SimpleDialogOption(
-                child: const Text('Choose from Gallery'),
-                onPressed: () async {
-                  Navigator.of(context).pop();
-                  PickedFile imageFile =
-                  await imagePicker.getImage(source: ImageSource.gallery, maxWidth: 1920, maxHeight: 1200, imageQuality: 80);
-                  setState(() {
-                    file = File(imageFile.path);
-                  });
-                }),
-            SimpleDialogOption(
-              child: const Text("Cancel"),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            )
-          ],
-        );
-      },
-    );
-  }
 
   void clearImage() {
     setState(() {
@@ -130,21 +83,70 @@ class _Uploader extends State<Uploader> {
   }
 }
 
-class PostForm extends StatelessWidget {
-  final imageFile;
+class PostForm extends StatefulWidget {
   final TextEditingController descriptionController;
   final TextEditingController locationController;
   final bool loading;
-  PostForm(
-      {this.imageFile,
-        this.descriptionController,
-        this.loading,
-        this.locationController});
+  PostForm({this.descriptionController, this.loading, this.locationController});
+
+  @override
+  _PostFormState createState() => _PostFormState();
+}
+
+class _PostFormState extends State<PostForm> {
+  ImagePicker imagePicker = ImagePicker();
+
+  _selectImage(BuildContext parentContext) async {
+    return showDialog<Null>(
+      context: parentContext,
+      barrierDismissible: false, // user must tap button!
+
+      builder: (BuildContext context) {
+        return SimpleDialog(
+          title: const Text('Create a Post'),
+          children: <Widget>[
+            SimpleDialogOption(
+                child: const Text('Take a photo'),
+                onPressed: () async {
+                  Navigator.pop(context);
+                  PickedFile imageFile = await imagePicker.getImage(
+                      source: ImageSource.camera,
+                      maxWidth: 1920,
+                      maxHeight: 1200,
+                      imageQuality: 80);
+                  setState(() {
+                    file = File(imageFile.path);
+                  });
+                }),
+            SimpleDialogOption(
+                child: const Text('Choose from Gallery'),
+                onPressed: () async {
+                  Navigator.of(context).pop();
+                  PickedFile imageFile = await imagePicker.getImage(
+                      source: ImageSource.gallery,
+                      maxWidth: 1920,
+                      maxHeight: 1200,
+                      imageQuality: 80);
+                  setState(() {
+                    file = File(imageFile.path);
+                  });
+                }),
+            SimpleDialogOption(
+              child: const Text("Cancel"),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            )
+          ],
+        );
+      },
+    );
+  }
 
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        loading
+        widget.loading
             ? LinearProgressIndicator()
             : Padding(padding: EdgeInsets.only(top: 0.0)),
         Divider(),
@@ -152,28 +154,37 @@ class PostForm extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: <Widget>[
             CircleAvatar(
-              backgroundImage: NetworkImage("https://www.trtspor.com.tr/resimler/366000/366896.jpg"),/*todo currentUserModel.photoUrl*/
+              backgroundImage: NetworkImage(
+                  "https://www.trtspor.com.tr/resimler/366000/366896.jpg"), /*todo currentUserModel.photoUrl*/
             ),
             Container(
               width: 250.0,
               child: TextField(
-                controller: descriptionController,
+                controller: widget.descriptionController,
                 decoration: InputDecoration(
                     hintText: "Write a caption...", border: InputBorder.none),
               ),
             ),
-            Container(
-              height: 45.0,
-              width: 45.0,
-              child: AspectRatio(
-                aspectRatio: 487 / 451,
-                child: Container(
-                  decoration: BoxDecoration(
-                      image: DecorationImage(
-                        fit: BoxFit.fill,
-                        alignment: FractionalOffset.topCenter,
-                        image: FileImage(imageFile),
-                      )),
+            GestureDetector(
+              onTap: () async {
+                _selectImage(context);
+              },
+              child: Container(
+                height: 45.0,
+                width: 45.0,
+                child: AspectRatio(
+                  aspectRatio: 487 / 451,
+                  child: Container(
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                      fit: BoxFit.fill,
+                      alignment: FractionalOffset.topCenter,
+                      image: file == null
+                          ? NetworkImage(
+                              "https://upload.wikimedia.org/wikipedia/commons/thumb/0/06/OOjs_UI_icon_add.svg/768px-OOjs_UI_icon_add.svg.png")
+                          : FileImage(file),
+                    )),
+                  ),
                 ),
               ),
             ),
@@ -185,7 +196,7 @@ class PostForm extends StatelessWidget {
           title: Container(
             width: 250.0,
             child: TextField(
-              controller: locationController,
+              controller: widget.locationController,
               decoration: InputDecoration(
                   hintText: "Where was this photo taken?",
                   border: InputBorder.none),
